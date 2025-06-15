a80595f9e57ca5e3f126e413e808ddb65722646d
binaryhustleio <shedebindra@gmail.com>
2025-06-14 18:26:17 -0700

    adding monitoring files

--- p4/bmv2/scripts/run_bmv.sh
+++ p4/bmv2/scripts/run_bmv.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# Simple BMv2 compile + run script
 
+P4FILE=src/switch1.p4
+JSONOUT=configs/basic.json
+
+echo "[*] compiling..."
+p4c-bm2-ss $P4FILE -o $JSONOUT
+
+if [ $? -ne 0 ]; then
+    echo "[!] Compilation failed"
+    exit 1
+fi
+
+echo "[*] running simple_switch with $JSONOUT"
+simple_switch $JSONOUT
